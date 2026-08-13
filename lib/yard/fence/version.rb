66d4b68c7dd96fa0566a7fc8b3af51f774b49830
Peter H. Boling <peter.boling@gmail.com>
2026-08-13 05:20:26 -0600

    🔖 Bump gem version

--- lib/yard/fence/version.rb
+++ lib/yard/fence/version.rb
@@ -5,7 +5,7 @@ module Yard
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = "0.9.8"
+      VERSION = "0.9.9"
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
